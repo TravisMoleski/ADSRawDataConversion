f6bd3ff9a1d02e07a0bf68fc9fc57833f8f75bd3
Autobuntu <rh166617@ohio.edu>
2023-12-08 14:21:47 -0500

    Cleanup

--- DisengagmentVideoExporter.py
+++ DisengagmentVideoExporter.py
@@ -11,14 +11,17 @@ class DesengagmentVideoExporter():
     def __init__(self):
         
         pass
-
-        
-    # Take in base64 string and return PIL image
+    
+    
+    # Take in base64 string and return PIL i
+    # mage
     def stringToImage(self, base64_string):
         imgdata = base64.b64decode(base64_string)
         return Image.open(io.BytesIO(imgdata))
+    
 
     def toRGB(self, image):
+        
         return cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
 
 
@@ -27,19 +30,6 @@ class DesengagmentVideoExporter():
         self.db = db
         self.auto_times = auto_times
         self.dt = dt
-
-
-        # # Set up MongoDB connection
-        # client = pymongo.MongoClient("mongodb://127.0.0.1:27017")
-
-        # # Access the database
-        # db = client.cyber19  # Replace 'your_database' with the actual name of your database
-
-        # # Access the collection (similar to a table in relational databases)
-        # collection = db.cyber19  # Replace 'your_collection' with the actual name of your collection
-
-        # # Query to extract data (you can customize this based on your needs)
-        # # query = {'topic': '/apollo/localization/pose'}
         
         for row in auto_times:
             
@@ -69,27 +59,7 @@ class DesengagmentVideoExporter():
                     rgb_im = self.toRGB(pil_im)
                     cv2.imshow(str(document['topic']), rgb_im)
                     cv2.waitKey(50)
-                    
-            
-            
+
             cv2.destroyAllWindows()
-                    
-            
-
-            # elif document['topic'] == '/apollo/sensor/velodyne32/PointCloud2':
-            #     # ax1.clear()
-            #     ax1.cla()
-            #     x_master = []
-            #     y_master = []
-            #     z_master = []
-            #     # print(document['point'])
-
-            #     for point in range(len(document['point'])):
-            #         x_master.append(document['point'][point]['x'])
-            #         y_master.append(document['point'][point]['y'])
-            #         z_master.append(document['point'][point]['z'])
-
-            #     ax1.scatter(x_master, y_master, z_master)
-            #     plt.pause(0.000000000001)
 
 
